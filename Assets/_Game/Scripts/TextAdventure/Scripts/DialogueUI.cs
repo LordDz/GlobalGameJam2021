a7511639bd13a0e78c27941d496c349f933455f8
david <david@DESKTOP-7NBMNN3>
2021-01-29 20:53:03 +0100

    Fix stuff

---
+++ Assets/_Game/Scripts/TextAdventure/Scripts/DialogueUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ggj.Assets._Game.Scripts.TextAdventure.Scripts
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            HideUI();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ShowUI()
+        {
+            this.enabled = true;
+        }
+
+        public void HideUI()
+        {
+            this.enabled = false;
+        }
+    }
+}
