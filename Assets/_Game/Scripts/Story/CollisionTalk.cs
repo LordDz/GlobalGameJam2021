33b34c1fee11ad8505e0c3d80a41a31afbbdd495
david <david@DESKTOP-7NBMNN3>
2021-01-30 19:27:59 +0100

    Updated dialogue.

---
+++ Assets/_Game/Scripts/Story/CollisionTalk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ggj.Assets._Game.Scripts.TextAdventure.Scripts
+{
+    public class CollisionTalk : MonoBehaviour
+    {
+        State currentState;
+        [SerializeField] State startingState;
+
+        ChoiceContainer choiceContainer;
+        DialogueUI dialogueUI;
+
+        bool hasStartedTalking = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            choiceContainer = FindObjectOfType<ChoiceContainer>();
+            dialogueUI = FindObjectOfType<DialogueUI>();
+            currentState = startingState;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!hasStartedTalking)
+            {
+                TalkToPlayer();
+            }
+        }
+
+        private void TalkToPlayer()
+        {
+            hasStartedTalking = true;
+            dialogueUI.ShowUI();
+            choiceContainer.SetTalk(currentState);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
