b6bb933e3e775744a177f55701b459eacb35c118
steddenhub <steddenhub>
2021-01-31 12:45:44 +0100

    Tram further tweaks.. it never ends

--- Assets/_Game/Scripts/VFX/VaryingFOV.cs
+++ Assets/_Game/Scripts/VFX/VaryingFOV.cs
@@ -7,13 +7,13 @@ namespace ggj
     public class VaryingFOV : MonoBehaviour
     {
     	//float m_FieldOfView;
-    	Vector2 maxMin = new Vector2(0.0f,60.0f);
+    	Vector2 maxMin = new Vector2(0.0f,53.0f);
     	
         // Start is called before the first frame update
         void Start()
         {
         	//m_FieldOfView = Camera.main.fieldOfView;
-        	maxMin = new Vector2(Camera.main.fieldOfView,maxMin[1]);
+        	maxMin = new Vector2(Camera.main.fieldOfView-5.0f,maxMin[1]);
         }
 
         // Update is called once per frame
